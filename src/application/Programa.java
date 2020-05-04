5591192d47c03f7a24c424659e71eb2fe0d87ed8
Thiago Vieira Nascimento <cpbadmin@CPB001174.cpb.org.br>
2020-05-04 11:33:11 -0300

    Classe de domínio Departamento

---
+++ src/application/Programa.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.domain.Departamento;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		
+		Departamento obj = new Departamento(1, "Livro");
+		
+		System.out.println(obj);
+	}
+
+}
